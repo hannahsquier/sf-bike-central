3b143f47810b16d934a30e2a65940271980364c8
Hannah Squier <hannahsquier@gmail.com>
2016-08-23 15:25:49 -0700

    order by id for incidents

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,6 @@
 class MapsController < ApplicationController
 	def show
 		@map_urls = Map.new(terrain: params[:terrain], bikeshare: params[:bikeshare]).get_map_urls
-		@incidents = Incident.all
+		@incidents = Incident.all.order(:id)
 	end
 end
